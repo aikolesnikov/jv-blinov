88f420ca1bc0d42e498adc9d3921dafdfd6f9f1b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-02-02 12:58:54 +0200

    init

---
+++ basics/src/introduction/Intro_1_2.java
@@ -0,0 +1,23 @@
+package introduction;
+
+
+// 1. 2. Отобразить в окне консоли аргументы командной строки в обратном порядке.
+public class Intro_1_2 {
+
+    public static void main(String[] args) {
+        for (String s : args
+                ) {
+            System.out.print(s);
+            System.out.println();
+        }
+
+
+	System.out.println();
+
+        for (int i = args.length-1; i >= 0; i--) {
+            System.out.print(args[i]);
+            System.out.println();
+        }
+
+    }
+}
