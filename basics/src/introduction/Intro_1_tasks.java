eb133482c50bb8047ede3068aaab96c5fd0caa1c
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-02-05 23:26:03 +0200

    tasks

---
+++ basics/src/introduction/Intro_1_tasks.java
@@ -0,0 +1,23 @@
+package introduction;
+
+/**
+ * book tasks
+ */
+public class Intro_1_tasks {
+
+    public static void main(String[] args) {
+        String s; // 1
+        if ((s = "java") == "java") {// 2
+            System.out.println (s+ " true");
+        } else {
+            System.out.println (s+ " false");
+        }
+
+        String ss;
+        System.out.println((ss="abc")=="abc");
+
+    }
+
+
+}
+
