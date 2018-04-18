40dba723887693baf404144a7001984e3bd01ab3
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-04-18 13:44:27 +0300

    datatypes 1_2

--- basics/src/datatypes/Tries.java
+++ basics/src/datatypes/Tries.java
@@ -3,10 +3,17 @@ package datatypes;
 import java.lang.reflect.Array;
 import java.math.BigDecimal;
 
-class MedicalStaff {}
-class Doctor extends MedicalStaff {}
-class Nurse extends MedicalStaff {}
-class HeadDoctor extends Doctor {}
+class MedicalStaff {
+}
+
+class Doctor extends MedicalStaff {
+}
+
+class Nurse extends MedicalStaff {
+}
+
+class HeadDoctor extends Doctor {
+}
 
 public class Tries {
 
@@ -42,23 +49,23 @@ public class Tries {
 
         Object var9 = 356f;
 
-        double x=0, y=2, z;
-        z = y/x;
-        System.out.println ("z="+z);
+        double x = 0, y = 2, z;
+        z = y / x;
+        System.out.println("z=" + z);
 
-        MedicalStaff medic = new HeadDoctor ();
+        MedicalStaff medic = new HeadDoctor();
         if (medic instanceof Nurse) {
-            System.out.println ("Nurse");
+            System.out.println("Nurse");
         } else if (medic instanceof Doctor) {
-            System.out.println ("Doctor");
+            System.out.println("Doctor");
         } else if (medic instanceof HeadDoctor) {
-            System.out.println ("HeadDoctor");
+            System.out.println("HeadDoctor");
         }
 
         System.out.println();
 
         int[] ar = new int[]{};
-        if (ar instanceof Object){
+        if (ar instanceof Object) {
             System.out.println("yes");
         }
 
@@ -66,6 +73,8 @@ public class Tries {
 
         char[] chAr = (new Integer("123")).toString().toCharArray();
         System.out.println(chAr.length);
+
+        int mas7 [] [] = new int [4] [];
 
     }
 }
